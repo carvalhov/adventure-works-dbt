1fbf173f468c1c216ee4ccd33a8d24c4db601d05
Vinicius Carvalho <vinicius.carvalho@indicium.tech>
2025-01-06 08:33:29 -0300

    marts documentation and dim_customers columns reorder

--- models/marts/dim_customer.sql
+++ models/marts/dim_customer.sql
@@ -4,5 +4,22 @@ with
         from {{ ref('int_customers_info') }}
     )
 
+    , marts as (
+        select
+            pk_customer_id
+            , fk_person_id
+            , fk_store_id
+            , fk_territory_id
+            , fk_sales_person_id
+            , dt_modified_at
+            , person_type
+            , person_name
+            , email_promotion
+            , store_name
+            , territory_name
+            , territory_group
+        from int_customers
+    )
+
 select *
-from int_customers
+from marts
